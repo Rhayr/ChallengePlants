8074d2a67d9b24db0f26560a4bcf772d0930f82e
Rhayra <rhayra.fiorentin@gmail.com>
2023-09-07 18:08:42 -0300

    feat: tab navigation

--- PlantParadise/App.js
+++ PlantParadise/App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet } from 'react-native';
 
 import Initial from './src/screens/initial';
 import Home from './src/screens/home';
+import Favorites from './src/screens/favorites';
+import Cart from './src/screens/cart';
 
 const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function HomeTab() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="Home" component={Home} />
+      <BottomTabs.Screen name="Favorites" component={Favorites} />
+      <BottomTabs.Screen name="Cart" component={Cart} />
+    </BottomTabs.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -19,7 +33,11 @@ export default function App() {
             component={Initial}
             options={{ headerShown: false }}
           />
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen
+            name="Home"
+            component={HomeTab}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
